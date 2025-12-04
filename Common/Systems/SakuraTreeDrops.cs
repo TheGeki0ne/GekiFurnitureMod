0065d4ee8f6b1613a7ea420cb405ade54f3fe79e
clayton and coal <clayton and coal@DESKTOP-VCM2LOB>
2025-12-03 23:11:14 -0600

    Add project files.

---
+++ Common/Systems/SakuraTreeDrops.cs
@@ -0,0 +1,33 @@
+﻿
+using Source.Content.Items;
+using Terraria;
+using Terraria.Audio;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+using Terraria.DataStructures;
+
+namespace Source.Common.Systems
+{
+    internal class GlobalTreeDrops : GlobalTile
+    {
+
+        public override bool CanDrop(int i, int j, int Type)
+        {
+            if (Type == TileID.VanityTreeSakura)
+            {
+                Item.NewItem(WorldGen.GetItemSource_FromTileBreak(i, j), i * 16, j * 16, 16, 16, ModContent.ItemType<SakuraWood>());
+                return false;
+            }
+            return base.CanDrop(i, j, Type);
+        }
+
+
+
+    }
+
+
+
+}
---
+++ Content/Items/SakuraWood.cs
@@ -0,0 +1,28 @@
+﻿using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.IO;
+using Terraria.ModLoader.Utilities;
+
+namespace Source.Content.Items
+{
+    internal class SakuraWood : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 22;
+
+            Item.maxStack = Item.CommonMaxStack;
+            Item.value = Item.buyPrice(0);
+        }
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("Sakura Wood");
+
+            Item.ResearchUnlockCount = 100;
+
+        }
+    }
+}
